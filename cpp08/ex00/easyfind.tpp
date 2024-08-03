0683844f3c54bac9e0eddccf72fc07697e2f63f4
Takumasa Okamoto <takuokam@c1r4s8.42tokyo.jp>
2024-08-03 17:52:51 +0900

    add deque and list

--- cpp08/ex00/easyfind.tpp
+++ cpp08/ex00/easyfind.tpp
@@ -1,4 +1,10 @@
 template <typename T>
 void EasyFind<T>::seekInt() {
-    std::cout << value << std::endl;
+    typename T::iterator it = std::find(value.begin(), value.end(), find);
+
+    if (it != value.end()) {
+        std::cout << "Value " << find << " found at position " << std::distance(value.begin(), it) << std::endl;
+    } else {
+        std::cout << "Value " << find << " not found" << std::endl;
+    }
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,6 +2,8 @@
 #include<string>
 #include<iostream>
 #include <vector>
+#include <list>
+#include <deque>
 
 int main()
 {
@@ -13,4 +15,22 @@ int main()
     numbers.push_back(5);
     EasyFind<std::vector<int> > c1(numbers, 3);
     c1.seekInt();
+
+    std::list<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    EasyFind<std::list<int> > c2(list, 3);
+    c2.seekInt();
+
+    std::deque<int> deque;
+    deque.push_back(1);
+    deque.push_back(2);
+    deque.push_back(3);
+    deque.push_back(4);
+    deque.push_back(5);
+    EasyFind<std::deque<int> > c3(deque, 3);
+    c3.seekInt();
 }
