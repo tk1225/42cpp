e7497805c30369d4f226a26589ad4e6b50d7fea8
Your Name <you@example.com>
2024-08-05 23:19:40 +0900

    finalize ex00

--- cpp08/ex00/easyfind.tpp
+++ cpp08/ex00/easyfind.tpp
@@ -1,10 +1,11 @@
 template <typename T>
 void EasyFind<T>::seekInt() {
-    typename T::iterator it = std::find(value.begin(), value.end(), find);
+  typename T::iterator it = std::find(value.begin(), value.end(), find);
 
-    if (it != value.end()) {
-        std::cout << "Value " << find << " found at position " << std::distance(value.begin(), it) << std::endl;
-    } else {
-        std::cout << "Value " << find << " not found" << std::endl;
-    }
+  if (it != value.end()) {
+    std::cout << "Value " << find << " found at position "
+              << std::distance(value.begin(), it) << std::endl;
+  } else {
+    std::cout << "Value " << find << " not found" << std::endl;
+  }
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,38 +1,36 @@
-#include "easyfind.hpp"
-#include<string>
-#include<iostream>
-#include <vector>
-#include <list>
 #include <deque>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::vector<int> numbers;
-    numbers.push_back(1);
-    numbers.push_back(2);
-    numbers.push_back(3);
-    numbers.push_back(4);
-    numbers.push_back(5);
-    EasyFind<std::vector<int> > c1(numbers, 3);
-    c1.seekInt();
+#include "easyfind.hpp"
 
-    std::list<int> list;
-    list.push_back(1);
-    list.push_back(2);
-    list.push_back(3);
-    list.push_back(4);
-    list.push_back(5);
-    EasyFind<std::list<int> > c2(list, 3);
-    c2.seekInt();
+int main() {
+  std::vector<int> numbers;
+  numbers.push_back(1);
+  numbers.push_back(2);
+  numbers.push_back(3);
+  numbers.push_back(4);
+  numbers.push_back(5);
+  EasyFind<std::vector<int> > c1(numbers, 3);
+  c1.seekInt();
 
-    std::deque<int> deque;
-    deque.push_back(1);
-    deque.push_back(2);
-    deque.push_back(3);
-    deque.push_back(4);
-    deque.push_back(5);
-    EasyFind<std::deque<int> > c3(deque, 3);
-    c3.seekInt();
+  std::list<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  list.push_back(5);
+  EasyFind<std::list<int> > c2(list, 3);
+  c2.seekInt();
 
-    // TODO: mapへの対応
+  std::deque<int> deque;
+  deque.push_back(1);
+  deque.push_back(2);
+  deque.push_back(3);
+  deque.push_back(4);
+  deque.push_back(5);
+  EasyFind<std::deque<int> > c3(deque, 3);
+  c3.seekInt();
 }
