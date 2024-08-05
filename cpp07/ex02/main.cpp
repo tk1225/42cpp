31adbae61ac822ffa94deb2aaa9459886b00bfaf
Your Name <you@example.com>
2024-08-05 22:55:20 +0900

    finalize ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -11,5 +11,22 @@ int main(void) {
   std::cout << std::endl;
 
   delete tmp;
+
+  Array<double>* tmp2 = new Array<double>(3);
+  for (unsigned int i = 0; i < 3; ++i) {
+    (*tmp2)[i] = i * 10;
+  }
+  for (unsigned int i = 0; i < 3; ++i) {
+    std::cout << (*tmp2)[i] << " ";
+  }
+  std::cout << std::endl;
+
+  try {
+    (*tmp2)[4];
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  delete tmp2;
+
   return 0;
 }
