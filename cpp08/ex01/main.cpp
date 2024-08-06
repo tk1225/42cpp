790dab98eebad7a72a320f3e29ca6a835a0fdeca
Your Name <you@example.com>
2024-08-06 22:08:17 +0900

    fix test

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -20,10 +20,12 @@ int main() {
   std::cout << sp.shortestSpan() << std::endl;
   std::cout << sp.longestSpan() << std::endl;
 
-  std::vector<int> vec(10000);
+  std::vector<int> vec(9999);
   std::iota(vec.begin(), vec.end(), 1);
   Span sp2 = Span(10000);
   sp2.insertNumbers(vec);
+  sp2.addNumber(10099);
+
   std::cout << sp2.shortestSpan() << std::endl;
   std::cout << sp2.longestSpan() << std::endl;
   return 0;
