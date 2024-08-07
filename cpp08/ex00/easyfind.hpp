672f9215ab4fc2faceaed146a7a03a4f1ac5a6fb
Your Name <you@example.com>
2024-08-07 23:01:25 +0900

    fix orthodox

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,18 +1,32 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include<string>
-#include<iostream>
 #include <algorithm>
+#include <iostream>
+#include <string>
 
 template <typename T>
 class EasyFind {
-private:
-    T value;
-    int find;
-public:
-    EasyFind(const T& val, int s) : value(val), find(s) {}
-    void seekInt();
+ private:
+  T value;
+  int find;
+
+ public:
+  EasyFind() {};
+  EasyFind(const EasyFind<T>& other) {
+    this->value = other.value;
+    this->find = other.find;
+  }
+  EasyFind& operator=(const EasyFind& EasyFindClass) {
+    if (this != &EasyFindClass) {
+      this->value = EasyFindClass.value;
+      this->find = EasyFindClass.find;
+    }
+    return *this;
+  }
+  ~EasyFind() {}
+  EasyFind(const T& val, int s) : value(val), find(s) {}
+  void seekInt();
 };
 
 #include "EasyFind.tpp"
